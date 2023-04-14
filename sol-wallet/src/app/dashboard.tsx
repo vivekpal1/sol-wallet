7bdea2b6bd8528b1cdb2949511742959f1ada5a7
Akshat Sharma <akshatcoder@outlook.com>
2023-04-14 19:23:12 +0000

    Apr 15, 2023, 12:53 AM

--- sol-wallet/src/app/dashboard.tsx
+++ sol-wallet/src/app/dashboard.tsx
@@ -1,66 +1,14 @@
-// pages/dashboard.tsx
 import { FC } from 'react'
 
 const DashboardPage: FC = () => {
   return (
-    <div className="bg-gray-50 min-h-screen flex flex-col">
-      <header className="bg-white py-4 shadow-md">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
-          <h1 className="text-xl font-bold">My Solana Wallet</h1>
-          <nav>
-            <ul className="flex space-x-4">
-              <li>
-                <a href="#" className="text-gray-500 hover:text-gray-600">
-                  Dashboard
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-blue-500 hover:text-blue-600">
-                  Transactions
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-blue-500 hover:text-blue-600">
-                  Settings
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <main className="flex-grow">
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <div className="bg-white p-4 rounded-lg shadow-md">
-              <h3 className="text-lg font-bold mb-2">Account Balance</h3>
-              <p className="text-gray-600">123.45 SOL</p>
-            </div>
-            <div className="bg-white p-4 rounded-lg shadow-md">
-              <h3 className="text-lg font-bold mb-2">Recent Transactions</h3>
-              <ul>
-                <li className="text-gray-600 mb-1">
-                  Sent 1.23 SOL to john.doe@gmail.com
-                </li>
-                <li className="text-gray-600 mb-1">
-                  Received 4.56 SOL from jane.doe@gmail.com
-                </li>
-              </ul>
-            </div>
-            <div className="bg-white p-4 rounded-lg shadow-md">
-              <h3 className="text-lg font-bold mb-2">Assets</h3>
-              <ul>
-                <li className="text-gray-600 mb-1">
-                  10.00 SOL
-                </li>
-                <li className="text-gray-600 mb-1">
-                  100.00 USDC
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </main>
+    <div className="bg-gray-50 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+        <p className="text-gray-600 mb-4">
+          Welcome to your Solana Wallet Dashboard! Here, you can manage your account and view your transactions.
+        </p>
+      </div>
     </div>
   )
 }
--- sol-wallet/src/app/page.tsx
+++ sol-wallet/src/app/page.tsx
@@ -1,37 +1,36 @@
-import { FC } from 'react'
-import Link from 'next/link'
-im
+import { FC, useState } from 'react';
+import { useClient } from 'blitz';
+import { Connection, SystemProgram } from '@solana/web3.js';
 
-const HomePage: FC = () => {
+const CreateWalletPage: FC = () => {
+  const [walletAddress, setWalletAddress] = useState<string>('');
+  const client = useClient();
+
+  const createWallet = async () => {
+    try {
+      const connection = new Connection("https://api.devnet.solana.com");
+      const payerAccount = await connection.requestAirdrop(walletAddress, 1000000000);
+      const createAccountIx = SystemProgram.createAccount({
+        fromPubkey: payerAccount.publicKey,
+        newAccountPubkey: walletAddress,
+        lamports: 1000000000,
+        space: 0,
+        programId: new PublicKey("11111111111111111111111111111111")
+      });
+      const tx = await client.solana.signAndSendTransaction(connection, createAccountIx, payerAccount);
+      console.log("Wallet created", tx);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div className="bg-gray-50 min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold mb-4">My Solana Wallet</h1>
-        <p className="text-gray-600 mb-4">
-          Welcome to My Solana Wallet! Use the navigation links to manage your Solana account.
-        </p>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <Link href="#" className="text-blue-500 hover:text-blue-600">
-                Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link href="#" className="text-blue-500 hover:text-blue-600">
-                Transactions
-              </Link>
-            </li>
-            <li>
-              <Link href="#" className="text-blue-500 hover:text-blue-600">
-                Settings
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div>
+      <h1>Create Wallet</h1>
+      <input type="text" value={walletAddress} onChange={e => setWalletAddress(e.target.value)} />
+      <button onClick={createWallet}>Create Wallet</button>
     </div>
   )
 }
 
-export default HomePage
+export default CreateWalletPage;
