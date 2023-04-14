37631d4962e5e44cbf49718f933d0b2a1d99f05b
Akshat Sharma <akshatcoder@outlook.com>
2023-04-14 20:19:08 +0000

    create wallet

--- sol-wallet/src/app/page.tsx
+++ sol-wallet/src/app/page.tsx
@@ -1,29 +1,39 @@
-import { FC } from 'react';
-import { Wallet } from 'multichain-crypto-wallet';
-import { useClient } from 'blitz';
+"use client";
+
+import { FC, useEffect } from 'react';
+import * as multichainWallet from 'multichain-crypto-wallet';
 
 const CreateWalletPage: FC = () => {
-  const client = useClient();
-
   const createWallet = async () => {
     try {
-      const wallet = await Wallet.create(client, 'solana');
+      const wallet = multichainWallet.createWallet({
+        derivationPath: "",
+        network: 'solana',
+      });
       console.log('Wallet created', wallet.address);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
+
+  useEffect(() => {
+    const button = document.querySelector('#createWalletButton');
+    button.addEventListener('click', createWallet);
+    return () => {
+      button.removeEventListener('click', createWallet);
+    };
+  }, []);
 
   return (
     <div className="bg-gray-50 min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-md">
         <h1 className="text-2xl font-bold mb-4">Create Wallet</h1>
-        <button onClick={createWallet} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        <button id="createWalletButton" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
           Create Wallet
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CreateWalletPage;
