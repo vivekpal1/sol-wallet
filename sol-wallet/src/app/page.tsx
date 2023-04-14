8d2f6a4f3d1966bb0537d3214e03272eaaf61e03
Akshat Sharma <akshatcoder@outlook.com>
2023-04-14 18:32:37 +0000

    Apr 15, 2023, 12:02 AM

--- sol-wallet/src/app/page.tsx
+++ sol-wallet/src/app/page.tsx
@@ -1,59 +1,35 @@
-// pages/index.tsx
 import { FC } from 'react'
 import Link from 'next/link'
 
+
 const HomePage: FC = () => {
   return (
-    <div className="bg-gray-50 min-h-screen flex flex-col">
-      <header className="bg-white py-4 shadow-md">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
-          <h1 className="text-xl font-bold">My Solana Wallet</h1>
-          <nav>
-            <ul className="flex space-x-4">
-              <li>
-                <Link href="/dashboard">
-                  <a className="text-blue-500 hover:text-blue-600">
-                    Dashboard
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/transactions">
-                  <a className="text-gray-500 hover:text-gray-600">
-                    Transactions
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/settings">
-                  <a className="text-gray-500 hover:text-gray-600">
-                    Settings
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <main className="flex-grow">
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <h2 className="text-2xl font-bold mb-4">
-            Welcome to My Solana Wallet
-          </h2>
-          <p className="text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            eget sagittis turpis. Aenean tristique ante at velit hendrerit, id
-            aliquam tortor vestibulum. Sed ac velit nulla. Suspendisse vitae
-            elit elit. Nullam euismod est sit amet orci luctus, sit amet
-            aliquet purus pulvinar.{' '}
-          </p>
-          <Link href="/dashboard">
-            <a className="text-blue-500 hover:text-blue-600">
-              Go to Dashboard
-            </a>
-          </Link>
-        </div>
-      </main>
+    <div className="bg-gray-50 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4">My Solana Wallet</h1>
+        <p className="text-gray-600 mb-4">
+          Welcome to My Solana Wallet! Use the navigation links to manage your Solana account.
+        </p>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="#" className="text-blue-500 hover:text-blue-600">
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="text-blue-500 hover:text-blue-600">
+                Transactions
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="text-blue-500 hover:text-blue-600">
+                Settings
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
